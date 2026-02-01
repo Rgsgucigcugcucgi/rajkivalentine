ff8626d331e216ada7f8ec9dda3b8c8c1a62a1d3
Rgsgucigcugcucgi <rajdep779@gmail.com>
2026-02-01 15:46:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const no = document.getElementById("no");
 const yes = document.getElementById("yes");
 const cat = document.getElementById("cat");
+const music = document.getElementById("bgm");
+
+document.body.addEventListener("click", () => {
+  music.play();
+});
 
 no.addEventListener("click", () => {
   const x = Math.random() * 150;
@@ -12,5 +17,20 @@ no.addEventListener("click", () => {
 yes.addEventListener("click", () => {
   cat.classList.remove("hide");
   alert("Yayyy 😍 Love you too 💖");
+  startHearts();
 });
 
+function startHearts() {
+  setInterval(() => {
+    const heart = document.createElement("div");
+    heart.className = "heart";
+    heart.innerHTML = "❤️";
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.fontSize = Math.random() * 20 + 15 + "px";
+    document.body.appendChild(heart);
+
+    setTimeout(() => {
+      heart.remove();
+    }, 5000);
+  }, 300);
+}
